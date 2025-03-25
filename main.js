1921ee999242dff8e10aac15bcabe6e274530f11
Nixiris <dark.killer91@hotmail.com>
2025-03-25 18:16:27 +1100

    license

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// ATOL Frontol 6 script for updating beer tap info from ISM using TrueAPI
+// Copyright (C) 2025 Nixiris Dartero
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Install ODBC:
 // https://sourceforge.net/projects/firebird/files/firebird-ODBC-driver/2.0.5-Release/Firebird_ODBC_2.0.5.156_Win32.exe/download
 

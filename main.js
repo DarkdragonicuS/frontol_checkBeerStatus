f325fe8db10030aa907fc11affb1403549736c49
Nixiris <dark.killer91@hotmail.com>
2025-03-27 10:00:49 +1100

    added saving tap ware name to userValues

--- main.js
+++ main.js
@@ -109,15 +109,18 @@ function fillUserValues(tapInfo)
         
         tapNumber = tap.TAP_NUMBER;
         tapName = tap.TAP_NAME;
+        wareName = tap.POS_WARE_NAME;
         
         expireDate = tapInfo[i].ExpirationDateStr;
         volumeData = tapInfo[i].volumeData;
 
         userValueNameTN = 'tapName_' + tapNumber;
+        userValueNameWN = 'wareName_' + tapNumber;
         userValueNameED = 'tapED_' + tapNumber;
         userValueNameVolume = 'tapVol_' + tapNumber;
 
         frontol.userValues.set(userValueNameTN, tapName);
+        frontol.userValues.set(userValueNameWN, tapNumber.toString() + " - " + wareName);
         frontol.userValues.set(userValueNameED, expireDate);
         frontol.userValues.set(userValueNameVolume, volumeData);
     }
@@ -338,3 +341,4 @@ function getBeerTapMark(tapCode)
 
 // для работы с json
 "object"!=typeof JSON&&(JSON={}),function(){"use strict";function f(e){return 10>e?"0"+e:e}function this_value(){return this.valueOf()}function quote(e){return rx_escapable.lastIndex=0,rx_escapable.test(e)?'"'+e.replace(rx_escapable,function(e){var t=meta[e];return"string"==typeof t?t:"\\u"+("0000"+e.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+e+'"'}function str(e,t){var r,n,u,f,o,a=gap,p=t[e];switch(p&&"object"==typeof p&&"function"==typeof p.toJSON&&(p=p.toJSON(e)),"function"==typeof rep&&(p=rep.call(t,e,p)),typeof p){case"string":return quote(p);case"number":return isFinite(p)?String(p):"null";case"boolean":case"null":return String(p);case"object":if(!p)return"null";if(gap+=indent,o=[],"[object Array]"===Object.prototype.toString.apply(p)){for(f=p.length,r=0;f>r;r+=1)o[r]=str(r,p)||"null";return u=0===o.length?"[]":gap?"[\n"+gap+o.join(",\n"+gap)+"\n"+a+"]":"["+o.join(",")+"]",gap=a,u}if(rep&&"object"==typeof rep)for(f=rep.length,r=0;f>r;r+=1)"string"==typeof rep[r]&&(n=rep[r],u=str(n,p),u&&o.push(quote(n)+(gap?": ":":")+u));else for(n in p)Object.prototype.hasOwnProperty.call(p,n)&&(u=str(n,p),u&&o.push(quote(n)+(gap?": ":":")+u));return u=0===o.length?"{}":gap?"{\n"+gap+o.join(",\n"+gap)+"\n"+a+"}":"{"+o.join(",")+"}",gap=a,u}}var rx_one=/^[\],:{}\s]*$/,rx_two=/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,rx_three=/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,rx_four=/(?:^|:|,)(?:\s*\[)+/g,rx_escapable=/[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,rx_dangerous=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta,rep;"function"!=typeof JSON.stringify&&(meta={"\b":"\\b","    ":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},JSON.stringify=function(e,t,r){var n;if(gap="",indent="","number"==typeof r)for(n=0;r>n;n+=1)indent+=" ";else"string"==typeof r&&(indent=r);if(rep=t,t&&"function"!=typeof t&&("object"!=typeof t||"number"!=typeof t.length))throw new Error("JSON.stringify");return str("",{"":e})}),"function"!=typeof JSON.parse&&(JSON.parse=function(text,reviver){function walk(e,t){var r,n,u=e[t];if(u&&"object"==typeof u)for(r in u)Object.prototype.hasOwnProperty.call(u,r)&&(n=walk(u,r),void 0!==n?u[r]=n:delete u[r]);return reviver.call(e,t,u)}var j;if(text=String(text),rx_dangerous.lastIndex=0,rx_dangerous.test(text)&&(text=text.replace(rx_dangerous,function(e){return"\\u"+("0000"+e.charCodeAt(0).toString(16)).slice(-4)})),rx_one.test(text.replace(rx_two,"@").replace(rx_three,"]").replace(rx_four,"")))return j=eval("("+text+")"),"function"==typeof reviver?walk({"":j},""):j;throw new SyntaxError("JSON.parse")})}();
+
